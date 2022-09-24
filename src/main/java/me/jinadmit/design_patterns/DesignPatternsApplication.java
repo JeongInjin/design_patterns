09018ddc6de28dcf2e7cdbcfdad379903133c345
injinjeong <snowwwwww@naver.com>
2022-09-24 21:15:30 +0900

    singleton1

--- src/main/java/me/jinadmit/design_patterns/DesignPatternsApplication.java
+++ src/main/java/me/jinadmit/design_patterns/DesignPatternsApplication.java
@@ -1,5 +1,6 @@
 package me.jinadmit.design_patterns;
 
+import me.jinadmit.design_patterns._01_creational_patterns._01_singleton.SingleTon1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesignPatternsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesignPatternsApplication.class, args);
+        SingleTon1 singleTon1 = SingleTon1.getInstance();
+        SingleTon1 singleTon2 = SingleTon1.getInstance();
+
+        System.out.println(singleTon1 == singleTon2); //true
+
+
     }
 
 }
